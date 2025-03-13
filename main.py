6ebed53cfc8fdf0070bd6b5f26e16cf81d62f3df
GianCestarolli <giancestarolli@gmail.com>
2025-03-12 22:04:41 -0300

    Novo_método
    Adição do método 'listar_finalizados'

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from models.finalizados import Finalizado
 
-o_hobbit = Finalizado("O Hobbit", "J.R Tolkien", 1937, 8)
+o_hobbit = Finalizado("O Hobbit", "J.R Tolkien", 1937, 8.0)
+o_livro_dos_cinco_aneis = Finalizado("O livro dos cinco anéis", "Miyamoto Musashi", 1645, 8.5)
+
 
 def main():
-    print(o_hobbit)
+    Finalizado.listar_finalizados()
 
 if __name__ == '__main__':
     main()
--- models/finalizados.py
+++ models/finalizados.py
@@ -1,9 +1,16 @@
 from models.livro import Livro
 
 class Finalizado(Livro):
+    livros_finalizados = []
     def __init__(self, nome, autor, ano, minha_nota):
         super().__init__(nome, autor, ano)
         self._minha_nota = minha_nota
+        Finalizado.livros_finalizados.append(self)
     
     def __str__(self):
         return super().__str__() + f'  {self._minha_nota}'
+    
+    @classmethod
+    def listar_finalizados(cls):
+        for livro in cls.livros_finalizados:
+            print (f'\n{livro._nome} | Autor: {livro._autor} | Ano de lançamento: {livro._ano} | Minha nota: {livro._minha_nota}\n')
