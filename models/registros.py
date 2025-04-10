af9197faff66448bb934b415b6b7ee7bd639f8c9
GianCestarolli <giancestarolli@gmail.com>
2025-04-10 07:50:55 -0300

    Adição do método 'salvar_dropados'

--- models/registros.py
+++ models/registros.py
@@ -25,6 +25,7 @@ class Registros:
         autorLivro = input("Digite o autor: ")
         motivoDrop = input("Digite o motivo do drop: ")
         Dropado(nomeLivro, autorLivro, motivoDrop)
+        Registros.salvar_dropados()
         os.system("cls")
         print("Livro registrado com sucesso!\n")
         input("Digite qualquer coisa para retornar: ")
@@ -51,5 +52,27 @@ class Registros:
         # Escreve a lista de dados atualizada no arquivo JSON
         with open("livros_finalizados.json", "w") as arquivo:
             json.dump(data_finalizados, arquivo, indent=4)
+
+    def salvar_dropados():
+        # Lê o arquivo JSON existente, se houver, para preservar os dados anteriores
+        try:
+            with open("livros_dropados.json", "r") as arquivo:
+                data_dropados = json.load(arquivo)
+        except (FileNotFoundError, json.JSONDecodeError):
+            # Caso o arquivo não exista ou esteja vazio, inicializa com uma lista vazia
+            data_dropados = []
+
+        # Adiciona os livros registrados na lista de dados
+        for livro in Dropado.livros_dropados:
+            data_dropados.append({
+                "nome": livro.nome,
+                "autor": livro.autor,
+                "motivo": livro.motivo
+            })
+        
+        # Escreve a lista de dados atualizada no arquivo JSON
+        with open("livros_dropados.json", "w") as arquivo:
+            json.dump(data_dropados, arquivo, indent=4)
+
 
 
